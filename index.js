85ef4947f9113fca2309e0de0b13746695137b50
khush1140 <khushbooheda32@gmail.com>
2025-07-20 17:24:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/hello', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello Upgrad!');
 });
 
 app.listen(3000, () => {
