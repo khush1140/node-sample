6f3cada42ac149d2caea46bb29c911486654b5bb
khush1140 <khushbooheda32@gmail.com>
2025-07-20 17:18:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
